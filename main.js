afbbf62b797a2cdaf4c04aea1a9722e0e899f4ae
Koshman-Dmitri <123456desher@rambler.ru>
2022-10-10 15:34:09 +0300

    Finished
    Adaptive, js, other corrections

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+const mobileNavBtn = document.querySelector('.mobileNav');
 const anchors = document.querySelectorAll('a[href*="#"]');
+const navList = document.querySelector('.nav__list');
 const navLinks = document.querySelectorAll('.nav__link');
 const priceItem = document.querySelectorAll('.price__item');
 const priceItemWrapper = document.querySelectorAll('.price-item__title-wrapper');
 const tabs = document.querySelectorAll('.tab');
 const featureContent = document.querySelectorAll('.feature__content');
+const content = document.querySelectorAll('[data-content]');
+
+//Кнопка мобильной навигации
+mobileNavBtn.addEventListener('click', () => {
+    mobileNavBtn.classList.toggle('active');
+    navList.classList.toggle('invisible');
+});
 
 // Плавная прокрутка
 for (let anchor of anchors) {
@@ -16,16 +25,30 @@ for (let anchor of anchors) {
             behavior: 'smooth',
             block: 'start'
         })
+
+        mobileNavBtn.classList.remove('active');
+        navList.classList.add('invisible');
     })
 }
 
-// Активная вкладка в навигации (НАДО НЕ ПО КЛИКУ, А ПО СКРОЛЛУ)
-// navLinks.forEach(function(link) {
-//     link.addEventListener('click', function() {
-//         navLinks.forEach(item => item.classList.remove('active'));
-//         this.classList.add('active');
-//     })
-// })
+
+// Анимация ссылок навигации (при нажатии и скролле)
+window.addEventListener('scroll', () => {
+    let scrollDistance = window.scrollY;
+
+    content.forEach((el, i) => {
+        if (el.offsetTop + el.clientHeight - document.querySelector('.nav-wrapper').clientHeight <= scrollDistance) {
+            navLinks.forEach((el) => el.classList.remove('active'))
+            
+            //Чтобы не было активной ссылки, когда блок Header
+            if (scrollDistance >= (content[1].offsetTop - content[0].offsetTop)) {
+                navLinks[i].classList.add('active');
+            }
+            
+        }
+    });
+});
+
 
 // Окна с табами
 tabs.forEach(function(tab) {
@@ -42,6 +65,7 @@ tabs.forEach(function(tab) {
     })
 })
 
+
 // Анимация цен
 priceItem.forEach(function(price) {
     price.addEventListener('mouseover', function() {
@@ -49,33 +73,6 @@ priceItem.forEach(function(price) {
         price.firstElementChild.classList.add('active');
     })
 })
-
-
-// Анимация скролла
-window.addEventListener('scroll', () => {
-    let scrollDistance = window.scrollY;
-    console.log(scrollDistance);
-
-    document.querySelectorAll('[data-content]').forEach((el, i) => {
-        if (el.offsetTop + el.clientHeight - document.querySelector('.nav-wrapper').clientHeight <= scrollDistance) {
-            navLinks.forEach((el) => {
-                if (el.classList.contains('active')) {
-                    el.classList.remove('active');
-                }
-            })
-
-            navLinks[i].classList.add('active');
-        }
-    });
-});
-
-
-
-
-
-
-
-
 
 
 
